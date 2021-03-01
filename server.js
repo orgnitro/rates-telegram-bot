588823eb8103afe3eb74a905c7cbbf346b629726
Semyon Dokiichuk <orgnitro@gmail.com>
2021-03-01 16:20:18 +0200

    refactoring

--- server.js
+++ server.js
@@ -50,6 +50,8 @@ db.close((err) => {
 // Server creation
 
 bot.launch();
-app.listen('3000', () => {
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log('Server started at port 3000');
 });
